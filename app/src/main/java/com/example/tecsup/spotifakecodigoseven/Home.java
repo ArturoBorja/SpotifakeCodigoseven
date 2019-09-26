691527b5dc8225a72819671655654d1246626192
dubcampos <53241023+dubcampos@users.noreply.github.com>
2019-09-25 19:39:19 -0500

    instancia Retrofit

--- app/src/main/java/com/example/tecsup/spotifakecodigoseven/Home.java
+++ app/src/main/java/com/example/tecsup/spotifakecodigoseven/Home.java
@@ -3,11 +3,21 @@ package com.example.tecsup.spotifakecodigoseven;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Home extends AppCompatActivity {
+
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.spotify.com/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
