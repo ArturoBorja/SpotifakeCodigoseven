90ae74d72a846fe2dca23040409d9692c16ed7e3
RAVC <rravc00@hotmail.com>
2019-10-11 22:05:31 -0500

    Login y Search interfaz
    interfaz lista, falta codigo.

--- app/src/main/java/com/example/tecsup/spotifakecodigoseven/Login.java
+++ app/src/main/java/com/example/tecsup/spotifakecodigoseven/Login.java
@@ -1,15 +1,13 @@
 package com.example.tecsup.spotifakecodigoseven;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_login);
     }
 }
